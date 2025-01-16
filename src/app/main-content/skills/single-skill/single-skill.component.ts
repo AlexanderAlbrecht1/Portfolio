b8d7650c76051a282cf62196c3285f3be0891cca
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-01-16 13:53:28 +0100

    skill set area

---
+++ src/app/main-content/skills/single-skill/single-skill.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+interface Skills {
+  name: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'app-single-skill',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './single-skill.component.html',
+  styleUrl: './single-skill.component.scss'
+})
+export class SingleSkillComponent {
+  skillList : Skills[] = [ {
+    name : 'HTML',
+    icon : 'html.svg'
+  },
+  {
+    name : 'CSS',
+    icon : 'css.svg'
+  },
+  {
+    name : 'JavaScript',
+    icon : 'javascript.svg'
+  },
+  {
+    name : 'Material Design',
+    icon : 'material-design.svg'
+  },
+  {
+    name : 'TypeScript',
+    icon : 'typescript.svg'
+  },  {
+    name : 'Angular',
+    icon : 'angular.svg'
+  },  {
+    name : 'Firebase',
+    icon : 'firebase.svg'
+  },  {
+    name : 'GIT',
+    icon : 'git.svg'
+  },  {
+    name : 'REST-Api',
+    icon : 'rest_api.svg'
+  },  {
+    name : 'Scrum',
+    icon : 'scrum.svg'
+  },  {
+    name : 'Growth mindset',
+    icon : 'growthMindset.svg'
+  }
+]
+}
--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SingleSkillComponent } from './single-skill/single-skill.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SingleSkillComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
