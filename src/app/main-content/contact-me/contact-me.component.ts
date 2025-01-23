6f3a98da6f1acb0b11b1c7ff2ec1dc32347077d9
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-01-23 10:39:39 +0100

    contact form validation

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,12 +1,28 @@
+import { FormsModule, NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+
+  contactData = {
+    name : '',
+    email : '',
+    message : '',
+    privacyPolicy: false,
+  }
+
+  onSubmit(ngFrom : NgForm) {
+    if(ngFrom.valid && ngFrom.submitted) {
+      console.log(this.contactData);
+    }
+
+
+  }
 
 }
