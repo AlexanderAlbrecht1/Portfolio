d27db032087a232247701055145d392633a7df51
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-01-23 11:24:27 +0100

    HTTP client inject

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     provideTranslateService({
       defaultLanguage: 'en',
     }),
