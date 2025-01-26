9b0b86a1a431f298fe82c85af5ee1b38429d8848
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-01-26 23:35:36 +0100

    references carousel logic

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -2,7 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SingleCommentComponent } from './single-comment/single-comment.component';
 
-
+interface Feedback {
+  id: number;
+  name: string;
+  role: string;
+  comment: string;
+}
 
 @Component({
   selector: 'app-references',
@@ -12,5 +17,56 @@ import { SingleCommentComponent } from './single-comment/single-comment.componen
   styleUrl: './references.component.scss',
 })
 export class ReferencesComponent {
+
+  feedbackList: Feedback[] = [
+    {
+      id: 1,
+      name: 'Florian Z.',
+      role: 'Team Partner',
+      comment:
+        'You have a friendly and calm demeanor, which makes you easy to work with in a team. Your structured way of working and your clean and understandable code mean that you manage to meet deadlines.',
+    },
+    {
+      id: 2,
+      name: 'Jörg D.',
+      role: 'Team Partner',
+      comment:
+        'A very active partner in the team who convinces with constructive ideas. His implementation was quick and reliable. If you have Alexander on your team, you can be sure that the project will not only succeed, but will also be good.I really enjoyed working with him and would be happy to work in a team with him again at any time.',
+    },
+    {
+      id: 3,
+      name: 'Firat T.',
+      role: 'Team Partner',
+      comment:
+        'You have a friendly and calm demeanor, which makes you easy to work with in a team. Your structured way of working and your clean and understandable code mean that you manage to meet deadlines.',
+    }
+  ];
+
+
+  currentIndex = 0;
+
+  next(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.feedbackList.length;
+  }
+
+  previous(): void {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.feedbackList.length) % this.feedbackList.length;
+  }
+
+  goToIndex(index: number): void {
+    this.currentIndex = index;
+  }
+
+  getVisibleFeedback() {
+    const prevIndex = (this.currentIndex - 1 + this.feedbackList.length) % this.feedbackList.length;
+    const nextIndex = (this.currentIndex + 1) % this.feedbackList.length;
+
+    return [
+      this.feedbackList[prevIndex],
+      this.feedbackList[this.currentIndex],
+      this.feedbackList[nextIndex]
+    ];
+  }
 
 }
--- src/app/main-content/references/single-comment/single-comment.component.ts
+++ src/app/main-content/references/single-comment/single-comment.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
-interface Feedback {
-  name: string;
-  role: string;
-  comment: string;
-}
+// interface Feedback {
+//   id: number;
+//   name: string;
+//   role: string;
+//   comment: string;
+// }
 
 @Component({
   selector: 'app-single-comment',
@@ -16,25 +17,55 @@ interface Feedback {
 })
 export class SingleCommentComponent {
 
-  feedbackList: Feedback[] = [
-    {
-      name: 'Florian Z.',
-      role: 'Team Partner',
-      comment:
-        'You have a friendly and calm demeanor, which makes you easy to work with in a team. Your structured way of working and your clean and understandable code mean that you manage to meet deadlines.',
-    },
-    {
-      name: 'Jörg D.',
-      role: 'Team Partner',
-      comment:
-        'A very active partner in the team who convinces with constructive ideas. His implementation was quick and reliable. If you have Alexander on your team, you can be sure that the project will not only succeed, but will also be good.I really enjoyed working with him and would be happy to work in a team with him again at any time.',
-    },
-    {
-      name: 'Firat T.',
-      role: 'Team Partner',
-      comment:
-        'You have a friendly and calm demeanor, which makes you easy to work with in a team. Your structured way of working and your clean and understandable code mean that you manage to meet deadlines.',
-    }
-  ];
+  // feedbackList: Feedback[] = [
+  //   {
+  //     id: 1,
+  //     name: 'Florian Z.',
+  //     role: 'Team Partner',
+  //     comment:
+  //       'You have a friendly and calm demeanor, which makes you easy to work with in a team. Your structured way of working and your clean and understandable code mean that you manage to meet deadlines.',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Jörg D.',
+  //     role: 'Team Partner',
+  //     comment:
+  //       'A very active partner in the team who convinces with constructive ideas. His implementation was quick and reliable. If you have Alexander on your team, you can be sure that the project will not only succeed, but will also be good.I really enjoyed working with him and would be happy to work in a team with him again at any time.',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Firat T.',
+  //     role: 'Team Partner',
+  //     comment:
+  //       'You have a friendly and calm demeanor, which makes you easy to work with in a team. Your structured way of working and your clean and understandable code mean that you manage to meet deadlines.',
+  //   }
+  // ];
+
+
+  // currentIndex = 0;
+
+  // next(): void {
+  //   this.currentIndex = (this.currentIndex + 1) % this.feedbackList.length;
+  // }
+
+  // previous(): void {
+  //   this.currentIndex =
+  //     (this.currentIndex - 1 + this.feedbackList.length) % this.feedbackList.length;
+  // }
+
+  // goToIndex(index: number): void {
+  //   this.currentIndex = index;
+  // }
+
+  // getVisibleFeedback() {
+  //   const prevIndex = (this.currentIndex - 1 + this.feedbackList.length) % this.feedbackList.length;
+  //   const nextIndex = (this.currentIndex + 1) % this.feedbackList.length;
+
+  //   return [
+  //     this.feedbackList[prevIndex], // Vorheriges Element
+  //     this.feedbackList[this.currentIndex], // Aktuelles Element
+  //     this.feedbackList[nextIndex] // Nächstes Element
+  //   ];
+  // }
 
 }
