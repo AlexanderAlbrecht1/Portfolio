bc9165afc354141b68a9f90c6034d1279c594989
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-01-30 10:10:11 +0100

    translate

--- src/app/main-content/landing-page/flowtext/flowtext.component.ts
+++ src/app/main-content/landing-page/flowtext/flowtext.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-flowtext',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './flowtext.component.html',
   styleUrl: './flowtext.component.scss'
 })
