7fe5fd62df585e34b6ebad18b471ee21be3ef93c
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-02-16 15:00:21 +0100

    added mobile menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { TranslationService } from '../services/translation.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
   currentLanguage: string;
 
   isChecked = false;
+
+  isVisible: boolean = false;
+
+  toggleVisibility(): void {
+    this.isVisible = !this.isVisible;
+  }
 
   constructor(private translationService: TranslationService) {
     this.currentLanguage = this.translationService.currentLanguage;
@@ -27,4 +33,15 @@ export class HeaderComponent {
     this.isChecked = newLang === 'de';
   }
 
+  toggleLanguageMobile(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.isChecked = inputElement.checked;
+    const newLang = this.isChecked ? 'de' : 'en';
+    this.translationService.switchLanguage(newLang);
+    this.currentLanguage = newLang;
+  }
+
+  stopEvent(event: Event): void {
+    event.stopPropagation();
+  }
 }
