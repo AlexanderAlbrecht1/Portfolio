f2604fb6cb2e287fb9f214b0830b48c361bd15b7
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-02-19 14:26:57 +0100

    custom cursor disable touch

--- src/app/shared/custom-cursor/custom-cursor.component.ts
+++ src/app/shared/custom-cursor/custom-cursor.component.ts
@@ -1,14 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-custom-cursor',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './custom-cursor.component.html',
   styleUrl: './custom-cursor.component.scss'
 })
 export class CustomCursorComponent {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  showCursor = true;
+
+  ngOnInit() {
+    this.showCursor = !this.isTouchDevice();
+    console.log('touch device', this.isTouchDevice());
+
+  }
+
+  isTouchDevice(): boolean {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+  }
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
